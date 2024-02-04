2cda445edb410208d4c44898bb8dc1e5302d2644
Andrew Meredith <ameredith@parsyl.com>
2024-02-03 23:23:08 -0700

    Do not attempt to add license comment above build flag

--- test/integration/example_test.go
+++ test/integration/example_test.go
@@ -1,3 +1,6 @@
+//go:build integration
+// +build integration
+
 /*
 Copyright 2024 Andrew Meredith
 
@@ -13,9 +16,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
-//go:build integration
-// +build integration
 
 package integration
 
--- tools/tools.go
+++ tools/tools.go
@@ -1,3 +1,5 @@
+//go:build tools
+
 /*
 Copyright 2024 Andrew Meredith
 
@@ -13,8 +15,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
-//go:build tools
 
 package tools
 
